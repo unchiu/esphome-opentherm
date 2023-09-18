4ebd9a400be0f56581dfa50ecfd3e1a0616d3541
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2023-09-18 23:26:06 +0300

    Additional logging for output

--- components/opentherm/output.h
+++ components/opentherm/output.h
@@ -19,6 +19,7 @@ public:
     void set_id(const char* id) { this->id = id; }
 
     void write_state(float state) override { 
+        ESP_LOGD("opentherm.output", "Received state: %.2f. Min value: %.2f, max value: %.2f", state, min_value, max_value);
         this->state = state < 0.003 && this->zero_means_zero_ ? 0.0 : min_value + state * (max_value - min_value);
         this->has_state_ = true;
         ESP_LOGD("opentherm.output", "Output %s set to %.2f", this->id, this->state);
